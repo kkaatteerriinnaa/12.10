1fbecf4571507ef0912414159c073ab3dc5e61dc
kkaat <kkaat@KATERINA>
2023-12-27 01:09:42 +0200

    Добавьте файлы проекта.

---
+++ WpfApp8/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CreditCalculatorApp
+{
+    public partial class MainWindow : Window
+    {
+        public static readonly DependencyProperty CreditAmountProperty =
+            DependencyProperty.Register("CreditAmount", typeof(double), typeof(MainWindow), new PropertyMetadata(0.0));
+
+        public double CreditAmount
+        {
+            get { return (double)GetValue(CreditAmountProperty); }
+            set { SetValue(CreditAmountProperty, value); }
+        }
+
+        public static readonly DependencyProperty MonthlyIncomeProperty =
+            DependencyProperty.Register("MonthlyIncome", typeof(double), typeof(MainWindow), new PropertyMetadata(0.0));
+
+        public double MonthlyIncome
+        {
+            get { return (double)GetValue(MonthlyIncomeProperty); }
+            set { SetValue(MonthlyIncomeProperty, value); }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
